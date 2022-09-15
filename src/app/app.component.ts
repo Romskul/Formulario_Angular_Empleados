516c2d891edddb1079f772f716713302748f6758
Romskul <inaki.martin.c@gmail.com>
2022-09-15 18:03:01 +0200

    formulario creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de empleados';
+
+  empleados:Empleado[] = [
+
+    new Empleado('Juan', 'Diaz', 'Presidente', 7500),
+    new Empleado('Pedro', 'Perez', 'Director', 6500),
+    new Empleado('Maria', 'Gomez', 'Jefa sección', 4500),
+    new Empleado('Ana', 'Lopez', 'Administrativa', 2500),
+
+  ]
+
+  agregarEmpleado(){
+
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+
+  }
+
+  cuadroNombre:string = "";
+  cuadroApellido:string = "";
+  cuadroCargo:string = "";
+  cuadroSalario:number = 0;
+
 }
